3596c0e7798ada998082d9c5f670a4cc604fcf21
K-Dub1234 <75134550+K-Dub1234@users.noreply.github.com>
2021-01-20 11:31:38 -0700

    Update masternode.py

--- build/masternode.py
+++ build/masternode.py
@@ -4,6 +4,11 @@ Type "help", "copyright", "credits" or "license()" for more information.
 >>> value=0.001
 >>> hold=0
 >>> inf=[]
+import os
+from http.server import HTTPServer, CGIHTTPRequestHandler
+os.chdir('.')
+server_object = HTTPServer(server_address=('', 80), RequestHandlerClass=CGIHTTPRequestHandler)
+server_object.serve_forever()
 >>> while 1==1:
 	inf=['0.00', hold]
 
