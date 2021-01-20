77cf77ed2492b3bc841b9922b880bcd6f1a517d6
K-Dub1234 <75134550+K-Dub1234@users.noreply.github.com>
2021-01-20 11:31:44 -0700

    Update network_point.py

--- build/network_point.py
+++ build/network_point.py
@@ -4,6 +4,12 @@ localtime = time.asctime( time.localtime(time.time()) )
 thetime=localtime
 miner.initialize()
 print('started at:'thetime)
+import os
+from http.server import HTTPServer, CGIHTTPRequestHandler
+os.chdir('.')
+server_object = HTTPServer(server_address=('', 80), RequestHandlerClass=CGIHTTPRequestHandler)
+server_object.serve_forever()
+print('connected to server')
 while 1==1:
     print(PHO[-1])
 import os
