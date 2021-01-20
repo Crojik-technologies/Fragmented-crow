125abdee4ec2307fa3293362374eea0b7c9890b9
K-Dub1234 <75134550+K-Dub1234@users.noreply.github.com>
2021-01-20 11:31:32 -0700

    Update __init__.py

--- FCROW21/__init__.py
+++ FCROW21/__init__.py
@@ -5,19 +5,49 @@ ip=0
 def setupNOD(ip):
     ip=random.randint(1111, 9999)
     ID=['NOD', '.', ip]
+    import os
+from http.server import HTTPServer, CGIHTTPRequestHandler
+os.chdir('.')
+server_object = HTTPServer(server_address=('', 80), RequestHandlerClass=CGIHTTPRequestHandler)
+server_object.serve_forever()
+
 
 def setupGHO(ip):
     ip=random.randint(1111, 9999)
     ID=['GHO', '.', ip]
+    import os
+from http.server import HTTPServer, CGIHTTPRequestHandler
+os.chdir('.')
+server_object = HTTPServer(server_address=('', 80), RequestHandlerClass=CGIHTTPRequestHandler)
+server_object.serve_forever()
+
 
 def setupEGH(ip):
     ip=random.randint(1111, 9999)
     ID=['EGH', '.', ip]
+    import os
+from http.server import HTTPServer, CGIHTTPRequestHandler
+os.chdir('.')
+server_object = HTTPServer(server_address=('', 80), RequestHandlerClass=CGIHTTPRequestHandler)
+server_object.serve_forever()
+
 
 def setupKIplus(ip):
     ip=random.randint(1111, 9999)
     ID=['KI+', '.', ip]
+    import os
+from http.server import HTTPServer, CGIHTTPRequestHandler
+os.chdir('.')
+server_object = HTTPServer(server_address=('', 80), RequestHandlerClass=CGIHTTPRequestHandler)
+server_object.serve_forever()
+
 
 def setupGminus(ip):
     ip=random.randint(1111, 9999)
     ID=['N/A', '.', ip]
+    import os
+from http.server import HTTPServer, CGIHTTPRequestHandler
+os.chdir('.')
+server_object = HTTPServer(server_address=('', 80), RequestHandlerClass=CGIHTTPRequestHandler)
+server_object.serve_forever()
+
